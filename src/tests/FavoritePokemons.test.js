1e6bdae42cdb7939b3a3e7ce4a4b8408755933cf
Bruna <brunac.cardososilva@gmail.com>
2022-03-22 16:52:28 -0300

    Requisito 3
    Em desenvolvimento
    
    Concluido.

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+test('se é exibido No favorite pokemon found, se não tiver pokémons favoritos', () => {
+  renderWithRouter(<FavoritePokemons />);
+
+  const fav = screen.getByText(/No favorite pokemon found/i);
+  expect(fav).toBeInTheDocument();
+});
+
+test('se é exibido todos os cards de pokémons favoritados', () => {
+  renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+  pokemons.forEach((pokemon) => {
+    const allPokemons = screen.getByText(pokemon.name);
+    expect(allPokemons).toBeInTheDocument();
+  });
+});
