b195957ef00d6cee3683a0f522cdfb652522d4db
Bruna <brunac.cardososilva@gmail.com>
2022-03-22 15:10:42 -0300

    Requisito 1
    Em desenvolvimento
    
    Concluido.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Se o primeiro link deve possuir o texto Home', () => {
+  renderWithRouter(<App />);
+  const routeHome = screen.getByText(/Home/i);
+  expect(routeHome).toBeInTheDocument();
+});
+
+test('Se o segundo link deve possuir o texto About', () => {
+  renderWithRouter(<App />);
+  const routeHome = screen.getByText(/About/i);
+  expect(routeHome).toBeInTheDocument();
+});
+
+test('Se terceiro link deve possuir o texto Favorite Pokémons', () => {
+  renderWithRouter(<App />);
+  const routeHome = screen.getByText(/Favorite Pokémons/i);
+  expect(routeHome).toBeInTheDocument();
+});
